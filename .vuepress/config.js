66211ae96cfebf306305f5c45610210d54f217cf
Hans Pagel <hans.pagel@ueber.io>
2018-09-12 23:47:43 +0200

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,60 @@
+module.exports = {
+    theme: 'craftdocs',
+    locales: {
+        '/': {
+            lang: 'en-US',
+            title: 'Learning Agile',
+        },
+    },
+    themeConfig: {
+        docsRepo: 'heyscrumpy/learn',
+        editLinks: true,
+        locales: {
+            '/': {
+                selectText: 'Language',
+                label: 'English',
+                editLinkText: 'Edit this page on GitHub',
+                sidebar: {
+                    '/': [
+                        {
+                            title: 'Getting Started',
+                            collapsable: false,
+                            children: [
+                                'introduction',
+                            ]
+                        },
+                        {
+                            title: 'Integrations',
+                            collapsable: false,
+                            children: [
+                                'github',
+                                'gitlab',
+                                'bitbucket',
+                                'slack',
+                            ]
+                        },
+                    ],
+                },
+            },
+        },
+        nav: [
+            {
+                text: 'Scrumpy',
+                items: [
+                    { text: 'Sign In', link: 'https://app.scrumpy.io/' },
+                    { text: 'Download Apps', link: 'https://scrumpy.io/apps' },
+                    { text: 'Changelog', link: 'https://scrumpy.io/changelog' },
+                    { text: 'Site Notice', link: 'https://scrumpy.io/imprint' },
+                    { text: 'Blog', link: 'https://medium.com/scrumpy' },
+                ]
+            },
+        ],
+    },
+    markdown: {
+        anchor: { level: [2, 3] },
+        config(md) {
+            let markup = require('vuepress-theme-craftdocs/markup')
+            md.use(markup)
+        }
+    }
+}
